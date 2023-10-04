1603bf0f266b03fc690ba59e3ba35b23929a3bfe
Rishop Babu <rishopbabu22@gmail.com>
2023-10-04 07:57:44 +0530

    expire minutes commented

--- app/oauth2.py
+++ app/oauth2.py
@@ -10,16 +10,16 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 SECRET_KEY = settings.secret_key
 ALGORITHM = settings.algorithm
-ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+#ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(
-        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
-    )  # always expiration time is in utc
-    to_encode.update({"exp": expire})
+    # expire = datetime.utcnow() + timedelta(
+    #     minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    # )  # always expiration time is in utc
+    # to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
